e91efe778c7b9c6a8b39d2b2612d6cb4987d3314
Jake Whatley <jacob.whatley91@gmail.com>
2014-10-21 20:57:45 +0100

    hlte: enable LTE by default
    Change-Id: I9f96b120713baa136f6fc748dc3fcc28122a5072

--- full_hlte.mk
+++ full_hlte.mk
@@ -18,6 +18,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from hlte device
 $(call inherit-product, device/samsung/hlte/device.mk)
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    telephony.lteOnGSMDevice=1 \
+    ro.telephony.default_network=9
+
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_hlte
 PRODUCT_DEVICE := hltexx
